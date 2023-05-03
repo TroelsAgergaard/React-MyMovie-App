0f256836c28e10c19d1ec080245ab0287fb5e4a8
TroelsAgergaard <troelsagergaard@gmail.com>
2023-05-03 11:01:01 +0200

    Reorganize App Views

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,12 @@
 import "./App.css";
-import MovieCard from "./templates/MovieCard";
-import Button from "./components/Button";
-import Heading from "./components/Heading";
-import Switch from "./components/Switch";
-import MovieItem from "./templates/MovieItem";
-import Navigation from "./components/Navigation";
 import DetailsView from "./pages/DetailsView";
+import ListView from "./pages/ListView";
 
 function App() {
   return (
-    <DetailsView />
-    // <div className="App">
-    //   <header className="gridContainer header">
-    //     <Switch justify="end" align="center" />
-    //     <Heading title="MyMovies" size="16" as="h1" />
-    //   </header>
-    //   <main>
-    //     <section className="HorizontalMovieListContainer">
-    //       <div className="flexContainer justify-space-between">
-    //         <Heading title="Now Showing" size="16" as="h2" />
-    //         <Button title="See More" />
-    //       </div>
-    //       <div className="flexContainer movieCardContainerLayout">
-    //         <MovieCard />
-    //         <MovieCard />
-    //         <MovieCard />
-    //         <MovieCard />
-    //       </div>
-    //     </section>
-    //     <section className="VerticalMovieListContainer">
-    //       <div className="flexContainer justify-space-between VerticalMovieListTitleLayout">
-    //         <Heading title="Popular" size="16" as="h2" />
-    //         <Button title="See More" />
-    //       </div>
-    //       <div className="flexContainer movieListContainerLayout">
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //         <MovieItem />
-    //       </div>
-    //     </section>
-    //   </main>
-    //   <footer>
-    //     <Navigation />
-    //   </footer>
-    // </div>
+    <div className="app">
+      <ListView />
+    </div>
   );
 }
 
---
+++ src/pages/ListView.jsx
@@ -0,0 +1,52 @@
+import MovieCard from "../templates/MovieCard";
+import Button from "../components/Button";
+import Heading from "../components/Heading";
+import Switch from "../components/Switch";
+import MovieItem from "../templates/MovieItem";
+import Navigation from "../components/Navigation";
+
+const ListView = () => {
+  return (
+    <>
+      <header className="gridContainer header">
+        <Switch justify="end" align="center" />
+        <Heading title="MyMovies" size="16" as="h1" />
+      </header>
+      <main>
+        <section className="HorizontalMovieListContainer">
+          <div className="flexContainer justify-space-between">
+            <Heading title="Now Showing" size="16" as="h2" />
+            <Button title="See More" />
+          </div>
+          <div className="flexContainer movieCardContainerLayout">
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+          </div>
+        </section>
+        <section className="VerticalMovieListContainer">
+          <div className="flexContainer justify-space-between VerticalMovieListTitleLayout">
+            <Heading title="Popular" size="16" as="h2" />
+            <Button title="See More" />
+          </div>
+          <div className="flexContainer movieListContainerLayout">
+            <MovieItem />
+            <MovieItem />
+            <MovieItem />
+            <MovieItem />
+            <MovieItem />
+            <MovieItem />
+            <MovieItem />
+            <MovieItem />
+          </div>
+        </section>
+      </main>
+      <footer>
+        <Navigation />
+      </footer>
+    </>
+  );
+};
+
+export default ListView;
